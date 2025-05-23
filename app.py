17a86078ceca44ba0e4ecc65ada019c2adad6b11
abhishek kumar chaurasia <abhishek.chaurasia2111@gmail.com>
2025-05-23 17:56:41 +0530

    Update app.py to run on 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 traffic_data = []
@@ -24,7 +25,6 @@ def collect():
 def live_data():
     return jsonify(traffic_data[-10:])  # Send last 10 visits
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
